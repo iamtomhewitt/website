2219dc95bf44b88048c2c635c28cd9222504d349
Tom Hewitt <tommhewitt@gmail.com>
2020-06-01 22:17:31 +0100

    style(footer): resize icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,14 +4,14 @@ import './Footer.css'
 
 export default class Footer extends Component {
 	render() {
+		const style = { height: 60, width: 60 }
 		return (
 			<div className="Footer">
-				<hr />
-				<SocialIcon className="icon" fgColor="#FFF" url="http://github.com/iamtomhewitt" />
-				<SocialIcon className="icon" fgColor="#FFF" url="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/" />
-				<SocialIcon className="icon" fgColor="#FFF" url="http://instagram.com/iamtomhewitt" />
-				<SocialIcon className="icon" fgColor="#FFF" url="https://www.youtube.com/channel/UCKP2LhsWT2UdXfUgY_yb8iw?" />
-				<SocialIcon className="icon" fgColor="#FFF" url="https://stackoverflow.com/users/3002268/tom?tab=profile" />
+				<SocialIcon style={style} className="icon" fgColor="#FFF" url="http://github.com/iamtomhewitt" />
+				<SocialIcon style={style} className="icon" fgColor="#FFF" url="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/" />
+				<SocialIcon style={style} className="icon" fgColor="#FFF" url="http://instagram.com/iamtomhewitt" />
+				<SocialIcon style={style} className="icon" fgColor="#FFF" url="https://www.youtube.com/channel/UCKP2LhsWT2UdXfUgY_yb8iw?" />
+				<SocialIcon style={style} className="icon" fgColor="#FFF" url="https://stackoverflow.com/users/3002268/tom?tab=profile" />
 			</div>
 		);
 	}
