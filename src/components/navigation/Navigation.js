cb50a05bac6b012755ae3411d7f68e45dd349739
Tom Hewitt <tommhewitt@gmail.com>
2020-06-01 21:54:15 +0100

    style(nav): restyle the navbar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import home from '../../images/home-icon.png';
 import './Navigation.css'
 
 export default class Navigation extends Component {
 	render() {
 		return (
-			<>
-				<Navbar bg="primary" variant="dark">
-					<Navbar.Brand href="#/">Home</Navbar.Brand>
-					<Nav className="mr-auto">
-						<Nav.Link href="#/experience">Experience</Nav.Link>
-						<Nav.Link href="#/github">Github</Nav.Link>
-						<Nav.Link href="#/contact">Contact</Nav.Link>
-					</Nav>
-				</Navbar>
-			</>
+			<div className="Navigation">
+				<ul>
+					<li><a href="#/"><img src={home} alt="img"></img></a></li>
+					<li><a href="#/experience">EXPERIENCE</a></li>
+					<li><a href="#/github">GITHUB</a></li>
+					<li><a href="#/contact">CONTACT</a></li>
+				</ul>
+			</div>
 		)
 	}
 }
