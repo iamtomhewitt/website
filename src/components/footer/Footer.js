2c50ead0d96f5e6ed152e4078821c37508140369
Tom Hewitt <tommhewitt@gmail.com>
2020-06-01 21:55:57 +0100

    refactor(footer): move footer to its own folder

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { SocialIcon } from 'react-social-icons';
+import './Footer.css'
+
+export default class Footer extends Component {
+	render() {
+		return (
+			<div className="Footer">
+				<hr />
+				<SocialIcon className="icon" fgColor="#FFF" url="http://github.com/iamtomhewitt" />
+				<SocialIcon className="icon" fgColor="#FFF" url="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/" />
+				<SocialIcon className="icon" fgColor="#FFF" url="http://instagram.com/iamtomhewitt" />
+				<SocialIcon className="icon" fgColor="#FFF" url="https://www.youtube.com/channel/UCKP2LhsWT2UdXfUgY_yb8iw?" />
+				<SocialIcon className="icon" fgColor="#FFF" url="https://stackoverflow.com/users/3002268/tom?tab=profile" />
+			</div>
+		);
+	}
+}
