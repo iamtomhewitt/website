c04d3ecee257fde1424587ceeee60b9dbc49d82e
Tom Hewitt <tommhewitt@gmail.com>
2020-06-01 22:16:38 +0100

    refactor(home): turn home into a functional component and minimilise

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+
 import "./Home.css";
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <div id="lander">
-                    <h1>Hi, I'm Tom!</h1>
-                    <div id="profile">
-                        <img src={process.env.PUBLIC_URL + '/images/Me.jpg'} alt=""/>
-                    </div>
-                    <p />
-                    <p>I'm currently a software developer at William Hill, working on the software for self service betting terminals. I play drums, a few video games (as well as make my own on Google Play!). And I love a bit of metal.</p>
-                    <p>I graduated from Nottingham Trent University with a First Class Honours in Computer Science (Games Technology)</p>
-                    <p>My final year project involved creating a virtual reality interactive model of the human heart to assess its usefulness as an alternative learning style for medical students (as compared to 2D diagrams and textbooks).</p>
-                </div>
-            </div>
-        );
-    }
+const Home = () => {
+	return (
+		<div className="Home">
+			<img src={process.env.PUBLIC_URL + '/images/Me.jpg'} alt="" />
+			<h1>Tom Hewitt</h1>
+			<h2>Software Developer</h2>
+			<hr />
+		</div>
+	);
 }
+
+export default Home;
