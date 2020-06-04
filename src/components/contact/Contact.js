74a3fd6de9f00e9273108d49f6936f2f58080169
Tom Hewitt <tommhewitt@gmail.com>
2020-06-04 20:02:58 +0100

    chore(contact): update css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import {
-  FormGroup, FormControl, FormLabel, Button,
-} from 'react-bootstrap';
 import * as emailjs from 'emailjs-com';
 
 import './Contact.css';
@@ -18,111 +15,102 @@ export default class Contact extends Component {
     };
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
 
-    handleChange = (event) => {
-      this.setState({
-        [event.target.id]: event.target.value,
-      });
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-    handleSubmit = (event) => {
-      event.preventDefault();
+    const {
+      email, firstName, lastName, message,
+    } = this.state;
 
-      const {
-        email, firstName, lastName, message,
-      } = this.state;
+    const parameters = {
+      from_name: email,
+      to_name: 'tommhewitt@gmail.com',
+      subject: `New Website Message from ${firstName} ${lastName}`,
+      message_html: message,
+    };
 
-      const parameters = {
-        from_name: email,
-        to_name: 'tommhewitt@gmail.com',
-        subject: `New Website Message from ${firstName} ${lastName}`,
-        message_html: message,
-      };
+    emailjs.send(
+      'gmail',
+      'template_bAqyBvgM',
+      parameters,
+      'user_D5T3rATIktVUiqjTa1fE0',
+    );
 
-      emailjs.send(
-        'gmail',
-        'template_bAqyBvgM',
-        parameters,
-        'user_D5T3rATIktVUiqjTa1fE0',
-      );
+    this.clearForm();
+  }
 
-      this.clearForm();
-    }
+  clearForm() {
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+    });
+  }
 
-    clearForm() {
-      this.setState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        message: '',
-      });
-    }
+  validateForm() {
+    const {
+      email, firstName, lastName, message,
+    } = this.state;
 
-    validateForm() {
-      const {
-        email, firstName, lastName, message,
-      } = this.state;
+    return (firstName.length > 0
+          && lastName.length > 0
+          && email.length > 0
+          && message.length > 0);
+  }
 
-      return (firstName.length > 0
-            && lastName.length > 0
-            && email.length > 0
-            && message.length > 0);
-    }
+  render() {
+    const {
+      email, firstName, lastName, message,
+    } = this.state;
 
-    render() {
-      const {
-        email, firstName, lastName, message,
-      } = this.state;
+    return (
+      <>
+        <div className="Contact">
+          <h1>Contact</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="entry">
+              <label>
+                First Name
+                <span className="required">*</span>
+              </label>
+              <input id="firstName" type="text" value={firstName} onChange={this.handleChange} />
+            </div>
 
-      return (
-        <>
-          <h1 id="title">Contact</h1>
-          <div id="container">
-            <form onSubmit={this.handleSubmit}>
-              <FormGroup controlId="firstName">
-                <FormLabel>First Name*</FormLabel>
-                <FormControl
-                  type="text"
-                  value={firstName}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <FormGroup controlId="lastName">
-                <FormLabel>Last Name*</FormLabel>
-                <FormControl
-                  type="text"
-                  value={lastName}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <FormGroup controlId="email">
-                <FormLabel>Email*</FormLabel>
-                <FormControl
-                  type="email"
-                  value={email}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <FormGroup controlId="message">
-                <FormLabel>Message*</FormLabel>
-                <FormControl
-                  autoFocus
-                  value={message}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
+            <div className="entry">
+              <label>
+                Last Name
+                <span className="required">*</span>
+              </label>
+              <input id="lastName" type="text" value={lastName} onChange={this.handleChange} />
+            </div>
 
-              <Button
-                block
-                variant="primary"
-                disabled={!this.validateForm()}
-                type="submit"
-              >
-                Send Message!
-              </Button>
-            </form>
-          </div>
-        </>
-      );
-    }
+            <div className="entry">
+              <label>
+                Email
+                <span className="required">*</span>
+              </label>
+              <input id="email" type="text" value={email} onChange={this.handleChange} />
+            </div>
+
+            <div className="entry">
+              <label>
+                Message
+                <span className="required">*</span>
+              </label>
+              <input id="message" type="text" value={message} onChange={this.handleChange} />
+            </div>
+
+            <button type="submit" disabled={!this.validateForm()}>Send!</button>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
