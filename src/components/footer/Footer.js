6eed5910e14d46628910eaa2c2a89d2f302f8fc0
Tom Hewitt <tommhewitt@gmail.com>
2020-06-04 21:17:28 +0100

    style(footer): remove social icons styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,15 +3,14 @@ import { SocialIcon } from 'react-social-icons';
 import './Footer.css';
 
 const Footer = () => {
-  const style = { height: 60, width: 60 };
   return (
     <div className="Footer">
       <hr />
-      <SocialIcon style={style} className="icon" fgColor="#FFF" url="http://github.com/iamtomhewitt" />
-      <SocialIcon style={style} className="icon" fgColor="#FFF" url="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/" />
-      <SocialIcon style={style} className="icon" fgColor="#FFF" url="http://instagram.com/iamtomhewitt" />
-      <SocialIcon style={style} className="icon" fgColor="#FFF" url="https://www.youtube.com/channel/UCKP2LhsWT2UdXfUgY_yb8iw?" />
-      <SocialIcon style={style} className="icon" fgColor="#FFF" url="https://stackoverflow.com/users/3002268/tom?tab=profile" />
+      <SocialIcon className="icon" fgColor="#FFF" url="http://github.com/iamtomhewitt" />
+      <SocialIcon className="icon" fgColor="#FFF" url="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/" />
+      <SocialIcon className="icon" fgColor="#FFF" url="http://instagram.com/iamtomhewitt" />
+      <SocialIcon className="icon" fgColor="#FFF" url="https://www.youtube.com/channel/UCKP2LhsWT2UdXfUgY_yb8iw?" />
+      <SocialIcon className="icon" fgColor="#FFF" url="https://stackoverflow.com/users/3002268/tom?tab=profile" />
     </div>
   );
 };
