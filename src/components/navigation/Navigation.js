8306968a581d7c411965b7621510af41099b7a95
Tom Hewitt <tommhewitt@gmail.com>
2020-08-03 20:57:34 +0100

    refactor: update the navigation bar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import home from '../../images/home-icon.png';
-import './Navigation.css';
+import './Navigation.scss';
+import { HyperlinkIcon } from '../icons/Hyperlink';
+import { Building } from '../icons/Building';
+import { Location } from '../icons/Location';
 
-const Navigation = () => (
-  <div className="Navigation">
-    <ul>
-      <li><a href="#/"><img src={home} alt="img" /></a></li>
-      <li><a href="#/experience">EXPERIENCE</a></li>
-      <li><a href="#/github">GITHUB</a></li>
-      <li><a href="#/contact">CONTACT</a></li>
-      <li><a href="#/about">ABOUT</a></li>
-    </ul>
-  </div>
-);
+const Navigation = props => {
+
+	const { currentPageName } = props;
+
+	return (
+		<div className="navigation">
+
+			<div className="navigation-profile">
+				<img src={`${process.env.PUBLIC_URL}/images/Me.jpg`} alt="" />
+				<ul className="navigation-profile-info">
+					<li><HyperlinkIcon/><span><a href="https://github.com/iamtomhewitt">Github</a></span></li>
+					<li><Building/><span>William Hill</span></li>
+					<li><Location/><span>Leeds</span></li>
+				</ul>
+			</div>
+
+			<div className="navigation-links">
+				<ul>
+					<li>Experience</li>
+					<li>Github</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+
+		</div>
+	);
+}
 
 export default Navigation;
