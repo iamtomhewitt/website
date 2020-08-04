983092aad0d6c1debfa6f7e95b30cdba4f422605
Tom Hewitt <tommhewitt@gmail.com>
2020-08-04 22:15:01 +0100

    refactor: fix background image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,20 +8,18 @@ const Home = () => (
       <div className="text">
         <h1>
           Hi, I'm
-          <span className="highlight"> Tom</span>
+          <span className="highlight">&nbsp;Tom</span>
           !
         </h1>
         <h2>Software Developer</h2>
         <p>
           I currently work at William Hill, working towards
-          <span className="highlight">full-stack development</span>
+          <span className="highlight">&nbsp;full-stack development</span>
           , on betting terminal software for betting shops.
         </p>
       </div>
 
-      <div className="image">
-        <img src={`${process.env.PUBLIC_URL}/images/Me.jpg`} alt="" />
-      </div>
+      <img src={`${process.env.PUBLIC_URL}/images/Me.jpg`} alt="" />
     </div>
   </div>
 );
