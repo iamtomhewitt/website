4030bbf2613b12c34ebb3e0655e73bcb2de376b4
Tom Hewitt <tommhewitt@gmail.com>
2020-08-04 21:46:56 +0100

    refactor: update home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import './Home.css';
+import Navigation from '../navigation/Navigation';
+import './Home.scss';
 
 const Home = () => (
-  <div className="Home">
-    <img src={`${process.env.PUBLIC_URL}/images/Me.jpg`} alt="" />
-    <h1>Tom Hewitt</h1>
-    <h2>Software Developer</h2>
+  <div className="home">
+    <Navigation />
+    <div className="content">
+      <div className="text">
+        <h1>
+          Hi, I'm
+          <span className="highlight"> Tom</span>
+          !
+        </h1>
+        <h2>Software Developer</h2>
+        <p>
+          I currently work at William Hill, working towards
+          <span className="highlight">full-stack development</span>
+          , on betting terminal software for betting shops.
+        </p>
+      </div>
+
+      <div className="image">
+        <img src={`${process.env.PUBLIC_URL}/images/Me.jpg`} alt="" />
+      </div>
+    </div>
   </div>
 );
 
