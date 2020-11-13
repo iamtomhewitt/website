74c424a183e786a5adb3bf70d2a637a15be25c8d
Tom Hewitt <tommhewitt@gmail.com>
2020-11-13 15:13:34 +0000

    refactor(#10): change home based on mobile device

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,35 +2,40 @@ import React from 'react';
 
 import './Home.scss';
 
-const Home = () => (
-  <div className="home">
-    <div className="content">
-      <div className="text">
-        <h1>
-          Hi, I'm
-          <span className="highlight">&nbsp;Tom</span>
-          !
-        </h1>
-        <h2>Software Developer</h2>
-        <p>
-          I currently work at William Hill, working towards
-          <span className="highlight">&nbsp;full-stack development</span>
-          , on terminal software for betting shops.
-        </p>
-        <div className="spotify">
-          <h4>Top Spotify Artists</h4>
-          <ul>
-            <li>Enter Shikari</li>
-            <li>Eminem</li>
-            <li>STARSET</li>
-            <li>The Naked And Famous</li>
-            <li>Biffy Clyro</li>
-          </ul>
+const Home = () => {
+  const isMobile = window.innerWidth < 480;
+  const getClass = (clazz) => (isMobile ? `${clazz}-mobile` : clazz);
+
+  return (
+    <div className={getClass('home')}>
+      <div className={getClass('content')}>
+        <div className={getClass('text')}>
+          <h1>
+            Hi, I'm
+            <span className="highlight">&nbsp;Tom</span>
+            !
+          </h1>
+          <h2>Software Developer</h2>
+          <p>
+            I currently work at William Hill, working towards
+            <span className="highlight">&nbsp;full-stack development</span>
+            , on terminal software for betting shops.
+          </p>
+          <div className={getClass('spotify')}>
+            <h4>Top Spotify Artists</h4>
+            <ul>
+              <li>Enter Shikari</li>
+              <li>Eminem</li>
+              <li>STARSET</li>
+              <li>The Naked And Famous</li>
+              <li>Biffy Clyro</li>
+            </ul>
+          </div>
         </div>
+        {!isMobile && <img src={`${process.env.PUBLIC_URL}/images/home.png`} alt="" />}
       </div>
-      <img src={`${process.env.PUBLIC_URL}/images/home.png`} alt="" />
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
