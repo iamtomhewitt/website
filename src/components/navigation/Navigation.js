fbc3afecaa31a530fbccd42e245f79e1c8786993
Tom Hewitt <tommhewitt@gmail.com>
2020-11-13 14:30:05 +0000

    refactor(#10): change navigation based on mobile device

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,11 +5,63 @@ import { InstagramIcon } from '../icons/Instagram';
 import { LinkedInIcon } from '../icons/LinkedIn';
 import { Location } from '../icons/Location';
 import { StackoverflowIcon } from '../icons/Stackoverflow';
+import { WorkIcon } from '../icons/Work';
+import { EmailIcon } from '../icons/Email';
+import { ToolIcon } from '../icons/Tool';
 import './Navigation.scss';
 
-const Navigation = () => (
-  <div className="navigation">
+const MobileLayout = () => (
+  <div className="navigation-mobile">
+    <div className="navigation-mobile">
+      <ul className="navigation-mobile-icons">
+        <li>
+          <a href="https://github.com/iamtomhewitt">
+            <GithubIcon />
+          </a>
+        </li>
+        <li>
+          <a href="http://instagram.com/iamtomhewitt">
+            <InstagramIcon />
+          </a>
+        </li>
+        <li>
+          <a href="https://stackoverflow.com/users/3002268/tom?tab=profile">
+            <StackoverflowIcon />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/">
+            <LinkedInIcon />
+          </a>
+        </li>
+      </ul>
 
+      <hr />
+
+      <ul className="navigation-mobile-icons">
+        <li>
+          <a href="#/experience">
+            <WorkIcon />
+          </a>
+        </li>
+        <li>
+          <a href="#/github">
+            <ToolIcon />
+          </a>
+        </li>
+        <li>
+          <a href="#/contact">
+            <EmailIcon />
+          </a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+);
+
+const DesktopLayout = () => (
+  <div className="navigation">
     <div className="navigation-profile">
       <a href="#/">
         <img src={`${process.env.PUBLIC_URL}/images/profile.png`} alt="" />
@@ -59,5 +111,11 @@ const Navigation = () => (
 
   </div>
 );
+
+const Navigation = () => {
+  const isMobile = window.innerWidth < 480;
+
+  return isMobile ? <MobileLayout /> : <DesktopLayout />;
+};
 
 export default Navigation;
