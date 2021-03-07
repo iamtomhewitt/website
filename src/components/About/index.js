234785af27b108af24ff84fa9652f160d3c34e6b
Tom Hewitt <tommhewitt@gmail.com>
2021-03-07 21:47:55 +0000

    chore: add about me

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import data from '../../data/home.json';
+
+import './About.scss';
+
+const About = () => (
+  <div className='about'>
+    <h2>About Me</h2>
+    <hr />
+    <p>I currently work at William Hill, working towards being a<span className="highlight">&nbsp;full-stack development</span>, on terminal software for betting shops.</p>
+    <div className='spotify'>
+      <h4>Top Spotify Artists</h4>
+      <ul>
+        {data.spotify.map((a, i) => <li key={i}>{a}</li>)}
+      </ul>
+    </div>
+  </div>
+);
+
+export default About;
