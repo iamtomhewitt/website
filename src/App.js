2fa8904b5de822be5ac85b5b23aa1bcfe6fcc67d
Tom Hewitt <tommhewitt@gmail.com>
2021-03-07 21:32:50 +0000

    chore: reinitialise

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import About from './components/About';
+import Intro from './components/Intro';
+// import './App.scss';
+
+const App = () => {
+  //https://www.chuckgroom.com/
+  return (
+    <div>
+      <section>
+        TODO - Nav bar
+      </section>
+
+      <section>
+        <Intro/>
+      </section>
+
+      <section>
+        <About/>
+      </section>
+
+      <section>
+        TODO - Worked at
+      </section>
+
+      <section>
+        TODO - Github repos
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Intro/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Intro.scss';
+
+const Intro = () => (
+  <div className='intro'>
+    <img src={`${process.env.PUBLIC_URL}/images/profile.png`} alt="" />
+    <h2>Tom Hewitt</h2>
+    <hr />
+    <h4>Software Developer</h4>
+  </div>
+);
+
+export default Intro;
