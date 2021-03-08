3f48000779a935f728501750f6079556a190c71d
Tom Hewitt <tommhewitt@gmail.com>
2021-03-08 20:11:31 +0000

    chore: add social icons and update css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,14 +3,14 @@ import data from '../../data/home.json';
 
 import './About.scss';
 
+const highlight = (text) => <span className="highlight">{text}</span>
+
 const About = () => (
   <div className="about">
     <h2>About Me</h2>
     <hr />
     <p>
-      I currently work at William Hill, working towards being a
-      <span className="highlight">&nbsp;full-stack development</span>
-      , on terminal software for betting shops.
+      I currently work at William Hill, working towards being a {highlight('full stack developer')}, on terminal software for betting shops. I play {highlight('drums')} and love {highlight('virtual reality gaming')}.
     </p>
     <div className="spotify">
       <h4>Top Spotify Artists</h4>
--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -8,7 +8,12 @@ const Intro = () => (
     <h2>Tom Hewitt</h2>
     <hr />
     <h4>Software Developer</h4>
-    <div>TODO SOCIAL ICONS</div>
+    <div className="icons">
+      <a href="https://github.com/iamtomhewitt"><img src="https://raw.githubusercontent.com/edent/SuperTinyIcons/master/images/svg/github.svg" /></a>
+      <a href="http://instagram.com/iamtomhewitt"><img src="https://raw.githubusercontent.com/edent/SuperTinyIcons/master/images/svg/instagram.svg" /></a>
+      <a href="https://www.linkedin.com/in/thomas-hewitt-ab7724a8/"><img src="https://raw.githubusercontent.com/edent/SuperTinyIcons/master/images/svg/linkedin.svg" /></a>
+      <a href="https://stackoverflow.com/users/3002268/tom?tab=profile"><img src="https://raw.githubusercontent.com/edent/SuperTinyIcons/master/images/svg/stackoverflow.svg" /></a>
+    </div>
   </div>
 );
 
