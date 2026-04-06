208aa8cd3902d4f180e82c6ff3cf41f7d57d0f00
Tom Hewitt <tommhewitt@gmail.com>
2026-04-06 12:04:00 +0100

    chore: add skills

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,27 +5,81 @@ import './index.scss';
 
 const About = () => {
   const quickLinks = [{
+    colour: '#fff',
     icon: 'github',
     label: 'Github',
-    colour: '',
   }, {
+    colour: '#0966C2',
     icon: 'linkedin',
     label: 'LinkedIn',
-    colour: '#0966C2',
   }, {
+    colour: '#EC7C24',
     icon: 'stack-overflow',
     label: 'Stack Overflow',
-    colour: '#EC7C24',
   }, {
+    colour: '#D62976',
     icon: 'instagram',
     label: 'Instagram',
-    colour: '#D62976',
   }];
 
   const skills = [{
-    icon: '',
-    label: 'TypeScript',
     colour: '#3074BF',
+    icon: 'typescript',
+    label: 'TypeScript',
+  }, {
+    colour: '#F0DB4F',
+    icon: 'js',
+    label: 'JavaScript',
+  }, {
+    colour: '#FF9900',
+    icon: 'aws',
+    label: 'AWS',
+  }, {
+    colour: '#ED8B00',
+    icon: 'java',
+    label: 'Java',
+  }, {
+    colour: '#BD1424',
+    icon: 'pied-piper-hat',
+    label: 'Jest',
+  }, {
+    colour: '#68A063',
+    icon: 'node',
+    label: 'NodeJS',
+  }, {
+    colour: '#306998',
+    icon: 'python',
+    label: 'Python',
+  }, {
+    colour: '#61DBFB',
+    icon: 'react',
+    label: 'React',
+  }, {
+    colour: '#FF4785',
+    folder: 'solid',
+    icon: 'book',
+    label: 'Storybook',
+  }, {
+    colour: '#fff',
+    icon: 'github',
+    label: 'Github',
+  }, {
+    colour: '#FC6D26',
+    icon: 'gitlab',
+    label: 'Gitlab',
+  }, {
+    colour: '#F1502F',
+    icon: 'git-alt',
+    label: 'Git',
+  }, {
+    colour: '#00758F',
+    folder: 'solid',
+    icon: 'database',
+    label: 'MySQL',
+  }, {
+    colour: '#0db7ed',
+    icon: 'docker',
+    label: 'Docker',
   }];
 
   return (
@@ -57,22 +111,22 @@ const About = () => {
 
           <h4>Skills</h4>
 
-          {/* <div>
-            {skills.map(skill => (
-              <div key={skill.label}>
-                {typeof skill.icon === 'string' ?
-                  <i
-                    className={`fa-xl fa-brands ${skill.icon}`}
-                    style={{
-                      color: skill.colour,
-                    }}
-                  /> :
-                  skill.icon}
+          <div className='about-skills'>
+            {skills
+              .sort((a, b) => a.label.localeCompare(b.label))
+              .map(skill => (
+                <div className='about-skill' key={skill.icon}>
+                  <LazySvg
+                    color={skill.colour}
+                    folder={(skill.folder as any) || 'brands'}
+                    height='27px'
+                    name={skill.icon}
+                  />
 
-                <span>{skill.label}</span>
-              </div>
-            ))}
-          </div> */}
+                  <div>{skill.label}</div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
 
