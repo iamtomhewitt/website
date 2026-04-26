7c6ac85b75ee22dbaf617b79b5f97bd4f89b73a3
Tom Hewitt <tommhewitt@gmail.com>
2026-04-26 19:03:31 +0100

    chore: remove the masthead

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 import About from '../About';
-import Masthead from '../Masthead';
 import Music from '../Music';
 import Projects from '../Projects';
 import Skills from '../Skills';
@@ -11,30 +9,22 @@ import pkg from '../../../package.json';
 
 import './index.scss';
 
-const App = () => {
-  const isMobile = useMediaQuery({
-    query: '(max-width: 430px)',
-  });
+const App = () => (
+  <div className='app'>
+    <div className='app-sections'>
+      <About />
 
-  return (
-    <div className='app'>
-      {!isMobile && <Masthead />}
+      <Music />
 
-      <div className='app-sections'>
-        <About />
+      <Skills />
 
-        <Music />
+      <Work />
 
-        <Skills />
-
-        <Work />
-
-        <Projects />
-      </div>
-
-      <div className='app-version'>{pkg.version}</div>
+      <Projects />
     </div>
-  );
-};
+
+    <div className='app-version'>{pkg.version}</div>
+  </div>
+);
 
 export default App;
